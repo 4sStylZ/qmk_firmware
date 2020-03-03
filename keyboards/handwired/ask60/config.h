a83ea186e9a1b5fadb01b94888dde02fd0615e9d
Noan MOUSY <4sstylz@protonmail.ch>
2020-03-03 17:52:26 +0100

    Trying to have a good tree

--- keyboards/handwired/ask60/config.h
+++ keyboards/handwired/ask60/config.h
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+// Commented for the teensy 3.2 compatibility.
 //#include "config_common.h"
 
 /* USB Device descriptor parameter */
